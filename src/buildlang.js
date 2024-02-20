d2658831e4f007d4254c90da090e3f67166c2087
christineli1412 <christineli1412@gmail.com>
2024-02-19 18:59:02 -0800

    added syntax check

--- src/buildlang.js
+++ src/buildlang.js
@@ -1,3 +1,10 @@
 import parse from "./parser.js"
 
-console.log(parse("print 1;"))
+try {
+  parse("print 1;")
+  console.log("Syntax ok")
+} catch (e) {
+  // log error message in red
+  console.error("\x1b[31m%s\x1b[0m", e.message)
+  process.exit(1)
+}
