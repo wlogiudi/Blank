90e752bbd2f66dca27ef89b7ae162414d40fb8f2
christineli1412 <christineli1412@gmail.com>
2024-02-19 19:05:32 -0800

    updated syntax check

--- src/buildlang.js
+++ src/buildlang.js
@@ -1,10 +1,19 @@
+import * as fs from "node:fs/promises"
+import process from "node:process"
 import parse from "./parser.js"
 
-try {
-  parse("print 1;")
-  console.log("Syntax ok")
-} catch (e) {
-  // log error message in red
-  console.error("\x1b[31m%s\x1b[0m", e.message)
-  process.exit(1)
+if (process.argv.length !== 3) {
+  console.log(
+    "Must have exactly one argument: the filename of the program to compile."
+  )
+} else {
+  try {
+    const buffer = await fs.readFile(process.argv[2])
+    parse(buffer.toString())
+    console.log("Syntax is ok")
+  } catch (e) {
+    // log error message in red
+    console.error("\x1b[31m%s\x1b[0m", e.message)
+    process.exit(1)
+  }
 }
